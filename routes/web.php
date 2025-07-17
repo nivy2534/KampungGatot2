306f7ff3442eae973b7fe2dadff743353d36816c
pascal <pascal@japati.net>
2025-07-17 21:36:04 +0700

    latest push

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,20 @@ Route::get('/', function () {
 Route::get('/blog', function () {
     return view('blog');
 })->name('blog');
+
+Route::get('/galeri', function () {
+    return view('galeri');
+})->name('galeri');
+
+Route::get('/blog/article', function () {
+    return view('article');
+})->name('article');
+
+Route::get('/belanja', function () {
+    return view('shop');
+})->name('shop');
+
+// Dummy single product page for all links
+Route::get('/produk-detail', function () {
+    return view('product');
+})->name('product');
